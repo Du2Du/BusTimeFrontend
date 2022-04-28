fb17792aa4b539b4e029705affa01511772f2e96
Du2Du <eduardohilario.eha@gmail.com>
2022-04-28 16:16:03 -0300

    Iniciando tela de registro

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { FixedHead } from "../../components";
+import styles from "../../styles/Register.module.scss";
 
 const Register: React.FC = () => {
-  return <div>Text</div>;
+  return (
+    <>
+      <FixedHead title="Registrar" />
+      <div className={styles.form}>hello</div>
+    </>
+  );
 };
 
 export default Register;
