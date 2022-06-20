135144855b9e696dc14c0f20f9a9c890d83557cf
Du2Du <eduardohilario.eha@gmail.com>
2022-06-20 12:22:51 -0300

    Adicionando campo de isAdmin em profile

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export { Pagination } from "./pagination";
 export { Button } from "./button";
 export { ButtonWithBorder } from "./button-with-border";
 export { SimpleInput } from "./simple-input";
+export { CheckboxInput } from "./checkbox";
