8074df7be889218d0d1e6d1478280bd4ffcf934a
Du2Du <eduardohilario.eha@gmail.com>
2022-12-04 13:54:56 -0300

    Voltando arquivo nextconfig

---
+++ next.config.js
@@ -0,0 +1,18 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  reactStrictMode: false,
+  cssModules: true,
+};
+
+module.exports = {
+  ...nextConfig,
+  ...require("next-transpile-modules")(["@amcharts/amcharts5"]),
+};
